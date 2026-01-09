fa38f1deb6bc77c8ff0e01290d1f7304d034e6af
efbka <erik.fernandez@institutvidreres.cat>
2026-01-09 11:39:41 +0100

    com3

---
+++ src/proyecto/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Menu} from '../menu/menu';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    Menu
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
---
+++ src/proyecto/perfil/perfil.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Menu} from '../menu/menu';
+
+@Component({
+  selector: 'app-perfil',
+  imports: [
+    Menu
+  ],
+  templateUrl: './perfil.html',
+  styleUrl: './perfil.css',
+})
+export class Perfil {
+
+}
