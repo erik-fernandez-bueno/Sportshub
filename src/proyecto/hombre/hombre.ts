b1bca5d7d9f1fc969d3688a5d554d12d285e2f16
efbka <erik.fernandez@institutvidreres.cat>
2026-01-23 12:18:41 +0100

    com9

--- src/proyecto/hombre/hombre.ts
+++ src/proyecto/hombre/hombre.ts
@@ -1,31 +1,22 @@
 import { Component } from '@angular/core';
-import { MenuComponent } from '../menu/menu';
-import { NgOptimizedImage } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-
-import { ProductoComponent } from '../producto/producto';
+import {MenuComponent} from '../menu/menu';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import  {ProductoComponent} from '../producto/producto'
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-hombre',
   imports: [
-    CommonModule,
     MenuComponent,
     NgOptimizedImage,
     RouterLink,
     FormsModule
   ],
   templateUrl: './hombre.html',
-  styleUrls: ['./hombre.css'],
+  styleUrl: './hombre.css',
   standalone: true,
 })
 export class HombreComponent {
-  products: ProductoComponent | undefined;
-  selectedCategory = 'todas';
 
-  get filteredProducts() {
-    if (this.selectedCategory === 'todas') return this.products;
-    return this.products.product((p: { category: string; }) => p.category === this.selectedCategory);
-  }
 }
