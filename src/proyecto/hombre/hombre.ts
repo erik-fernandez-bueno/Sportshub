4d3b20366f34ccb2d53aaa074ab10ecaee526c49
efbka <erik.fernandez@institutvidreres.cat>
2026-01-23 12:14:48 +0100

    com9

--- src/proyecto/hombre/hombre.ts
+++ src/proyecto/hombre/hombre.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
-import {MenuComponent} from '../menu/menu';
-import {NgOptimizedImage} from '@angular/common';
-import {RouterLink} from '@angular/router';
+import { MenuComponent } from '../menu/menu';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { ProductoComponent } from '../producto/producto';
 
 @Component({
   selector: 'app-hombre',
   imports: [
+    CommonModule,
     MenuComponent,
     NgOptimizedImage,
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './hombre.html',
-  styleUrl: './hombre.css',
+  styleUrls: ['./hombre.css'],
   standalone: true,
 })
 export class HombreComponent {
+  products: ProductoComponent | undefined;
+  selectedCategory = 'todas';
 
+  get filteredProducts() {
+    if (this.selectedCategory === 'todas') return this.products;
+    return this.products.product((p: { category: string; }) => p.category === this.selectedCategory);
+  }
 }
--- src/proyecto/producto/producto.ts
+++ src/proyecto/producto/producto.ts
@@ -28,7 +28,8 @@ export class ProductoComponent implements OnInit {
       description: 'Ofrecen un talle alto que te da libertad de movimiento, 3 bolsillos prácticos y una cinta para tu cepillo.',
       price: 44.99,
       image: 'assets/Pantalonvaquerohombre.jpg',
-      size: ['38', '40', '42', '44', '46', '48']
+      size: ['38', '40', '42', '44', '46', '48'],
+      category: 'pantalon'
     },
     {
       id: 2,
@@ -36,7 +37,8 @@ export class ProductoComponent implements OnInit {
       price: 1200,
       image: 'assets/Bicicletademontanya29.jpg',
       description: 'Tienen dos ruedas.',
-      size:  []
+      size:  [],
+      category: 'vehiculo'
     }
   ];
 
